dfeb1146bf74a4523b7c2c103123f781ccee45e4
MOHD SALMAN <salman1256@yahoo.com>
2024-07-31 17:53:22 +0530

    Day 6 Content

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,7 +1,15 @@
+using AutoMapper;
+using CommandService.Data;
+using CommandService.Dto;
+using CommandService.Repos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options=>options.UseInMemoryDatabase("InMemory"));
+builder.Services.AddScoped<ICommandRepo,CommandRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
